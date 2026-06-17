7329b9cb0e53f25170aa99a62c282d2ee431d409
Henri Payno <henri.payno@esrf.fr>
2026-06-17 15:53:05 +0200

    Doc: fix link to the ewoks logo for html_theme_options

--- doc/conf.py
+++ doc/conf.py
@@ -65,7 +65,7 @@ html_theme_options = {
         {
             "name": "ewoks",
             "url": "https://ewoks.readthedocs.io/en/stable",
-            "icon": "https://gitlab.esrf.fr/workflow/ewoksadmin/ewoksdoc/-/raw/main/logos/ewoks_for_sphinx_theme.svg",  # This svg contains dark / light mode information
+            "icon": "https://gitlab.esrf.fr/workflow/ewoksadmin/ewoksdoc/-/raw/main/doc/_static/ewoks_for_sphinx_theme.svg",  # This svg contains dark / light mode information
             "type": "local",
         },
     ],
